0fb3f834e8074b956effea213bf2247abb0dea90
innerNULL <lyt697@outlook.com>
2024-05-28 13:18:44 +0800

    fix: Compiling bugs

--- bin/deploy/tersorrt-llm/deploy.sh
+++ bin/deploy/tersorrt-llm/deploy.sh
@@ -33,7 +33,8 @@ function build_py_runtime() {
   if [ -d "${PY_RUNTIME}" ]; then
     echo "Python env '${PY_RUNTIME}' is already there"
   else
-    python3 -m venv ${PY_RUNTIME} --copies
+    ${CONDA} create --prefix ${PY_RUNTIME} python=3.10 --yes
+    rm ${PY_RUNTIME}/compiler_compat/ld
     ${PYTHON} -m pip install --upgrade pip
     ${PYTHON} -m pip install tensorrt_llm==${TENSORRT_LLM_VERSION} -U --extra-index-url https://pypi.nvidia.com
   fi
@@ -46,22 +47,23 @@ function download_model() {
   ${HF_CLI} login --token ${HF_TOKEN}
   cd ${CURR_DIR}
   cd ${MODEL_DIR}
-  git clone https://${HF_USER}:${HF_TOKEN}@huggingface.co/${LLM_VERSION}
+  git clone --progress --verbose https://${HF_USER}:${HF_TOKEN}@huggingface.co/${LLM_VERSION}
 }
 
 
 function build_model() {
   cd ${CURR_DIR}
   local model_path=${MODEL_DIR}/$(echo ${LLM_VERSION} | awk -F'/' '{print $NF}')
+  local data_type="float16"
+
   ${PYTHON} ${WORKSPACE}/TensorRT-LLM/examples/llama/convert_checkpoint.py \
     --model_dir ${model_path} \
     --output_dir ${model_path}_merged \
-    --dtype bfloat16 \
-    --load_model_on_cpu
+    --dtype ${data_type} 
 
   ${PY_RUNTIME}/bin/trtllm-build \
     --checkpoint_dir ${model_path}_merged \
-    --gemm_plugin bfloat16 \
+    --gemm_plugin ${data_type} \
     --output_dir ${model_path}_converted
 }
 
