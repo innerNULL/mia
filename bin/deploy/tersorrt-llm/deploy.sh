6e0c25fef557987a39a946eb519e7abce8a2a90e
innerNULL <lyt697@outlook.com>
2024-06-25 09:23:15 +0800

    chore: Backup tensor-rt deployment script

--- bin/deploy/tersorrt-llm/deploy.sh
+++ bin/deploy/tersorrt-llm/deploy.sh
@@ -7,7 +7,7 @@ set -x
 source $1
 
 CURR_DIR=$(pwd)
-WORKSPACE="./_workspace"
+WORKSPACE="${CURR_DIR}/_workspace"
 PY_RUNTIME="${WORKSPACE}/pyenv"
 PYTHON=${PY_RUNTIME}/bin/python
 HF_CLI=${PY_RUNTIME}/bin/huggingface-cli
@@ -25,6 +25,7 @@ function download_trtllm_src() {
   cd ${CURR_DIR}
   cd ${WORKSPACE}
   git clone -b v${TENSORRT_LLM_VERSION} https://github.com/NVIDIA/TensorRT-LLM.git
+  git clone -b v${TRITON_LLM_VERSION} https://github.com/triton-inference-server/tensorrtllm_backend.git
 }
 
 
@@ -54,17 +55,46 @@ function download_model() {
 function build_model() {
   cd ${CURR_DIR}
   local model_path=${MODEL_DIR}/$(echo ${LLM_VERSION} | awk -F'/' '{print $NF}')
-  local data_type="float16"
+  
+  if [ -d "${model_path}_converted" ]; then
+    echo "Path ${model_path}_converted already exists"
+  else
+    ${PYTHON} ${WORKSPACE}/TensorRT-LLM/examples/llama/convert_checkpoint.py \
+      --model_dir ${model_path} \
+      --output_dir ${model_path}_merged \
+      --dtype ${DATA_TYPE}
 
-  ${PYTHON} ${WORKSPACE}/TensorRT-LLM/examples/llama/convert_checkpoint.py \
-    --model_dir ${model_path} \
-    --output_dir ${model_path}_merged \
-    --dtype ${data_type} 
+    ${PY_RUNTIME}/bin/trtllm-build \
+      --checkpoint_dir ${model_path}_merged \
+      --gemm_plugin ${DATA_TYPE} \
+      --output_dir ${model_path}_converted
 
-  ${PY_RUNTIME}/bin/trtllm-build \
-    --checkpoint_dir ${model_path}_merged \
-    --gemm_plugin ${data_type} \
-    --output_dir ${model_path}_converted
+    rm -rf ${model_path}_merged
+  fi
+  
+  local converted_model_dir=$(cd ${model_path}_converted && pwd)
+  cd ${WORKSPACE}/tensorrtllm_backend
+  cp ${converted_model_dir}/rank0.engine ./all_models/inflight_batcher_llm/tensorrt_llm/1/
+  
+  local triton_model_dir="$(pwd)/all_models/inflight_batcher_llm/tensorrt_llm"
+  local inf_modes="decoupled_mode:true"
+  inf_modes="${inf_modes},engine_dir:/all_models/inflight_batcher_llm/tensorrt_llm/1"
+  inf_modes="${inf_modes},max_tokens_in_paged_kv_cache:"
+  inf_modes="${inf_modes},batch_scheduler_policy:guaranteed_completion"
+  inf_modes="${inf_modes},kv_cache_free_gpu_mem_fraction:0.2"
+  inf_modes="${inf_modes},max_num_sequences:4"
+  
+  ${PYTHON} tools/fill_template.py --in_place \
+    all_models/inflight_batcher_llm/tensorrt_llm/config.pbtxt \
+    ${inf_modes}
+  
+  ${PYTHON} tools/fill_template.py --in_place \
+    all_models/inflight_batcher_llm/preprocessing/config.pbtxt \
+    tokenizer_type:llama,tokenizer_dir:${LLM_VERSION}
+
+  ${PYTHON} tools/fill_template.py --in_place \
+    all_models/inflight_batcher_llm/postprocessing/config.pbtxt \
+    tokenizer_type:llama,tokenizer_dir:${LLM_VERSION}
 }
 
 
